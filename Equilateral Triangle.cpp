8f370b35101cb1438fc7d6271c7caebc1660180f
Jahanvi Sharma <56484763+jahanvisharma-dotcom@users.noreply.github.com>
2021-01-18 23:45:42 +0530

    Create Equilateral Triangle.cpp

---
+++ Equilateral Triangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main2(){
+	cout << "\t\tC++ program to Calculate Area of Equilateral Triangle.\n ";
+	float a, Area;
+	cout << "Please enter the value for the side of Triangle: ";
+	cin >> a;
+	if (a > 0) {
+		Area = sqrt(2) * a * a * 0.25;
+		cout << "The Area of this triangle is " << Area << ";" << endl;
+	}
+	else {
+		cout << "The sides of Triangle cannot be negative numbers ! " << endl;
+	}
+	system("pause");
+	return 0;
+}
