090ce3dbb90edbb261648244952219c88a0927ed
Jahanvi Sharma <56484763+jahanvisharma-dotcom@users.noreply.github.com>
2021-01-22 23:00:27 +0530

    Create Swap Using Functions.cpp

---
+++ Swap Using Functions.cpp
@@ -0,0 +1,14 @@
+//Program to swap two values using functions
+
+#include <iostream>
+#include <math.h>//C++ libriry which is calculating "pow" and PI=3.14
+using namespace std;
+int main2(){
+	int a, b;
+	cout << "Enter two numbers: ";
+	cin >> a >> b;
+	swap(a, b);//C++ function to swap two numbers 
+	cout << a << " & " << b << endl; //swap is the function which is contained in c++ ==>
+	                                 //==> there is no need to open new function to swap
+	return 0;
+}//ending the function main
